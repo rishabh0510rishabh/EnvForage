dc50a759e4efc12d2b751f9d5de6a62910b3a82d
Priyanshu <upscias0103@gmail.com>
2026-06-10 14:59:08 +0530

    style(backend): manually fix linting errors in config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,8 @@
-from typing import Annotated, Any, Literal
 from pathlib import Path
-from pydantic import BeforeValidator, Field, field_validator, model_validator
+from typing import Annotated, Any, Literal
+
+from dotenv import load_dotenv
+from pydantic import BeforeValidator, field_validator, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Helper function for CORS
